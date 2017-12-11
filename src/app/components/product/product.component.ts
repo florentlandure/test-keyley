a32d02cf70d21a2b5154245662350730f2d544f2
Florent Landuré <f.landure@gmail.com>
2017-12-11 17:50:45 +0100

    Product description page done

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Product from '../../interfaces/product.interface';
+import { ProductsDataService } from '../../services/products/products-data.service';
+import Category from '../../interfaces/category.interface';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  product: Product;
+  selectedImage: string;
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsDataService
+  ) { }
 
   ngOnInit() {
+    const subscription = this.route.params.subscribe(param => {
+      const id = param.id;
+      this.product = this.productsService.getProductById(parseInt(id, 10));
+      this.categories = this.product.categories
+        .map(category => this.productsService.getCategoryById(category));
+      this.selectedImage = this.product.images[0] || '';
+
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    });
+  }
+
+  changeSelectedImage(id: number): void {
+    this.selectedImage = this.product.images[id];
   }
 
 }
